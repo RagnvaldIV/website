bdb1bea970bcf0fc86c6f1a22ba8742770930570
Nicolas Bernt <nicolasbernt@gmail.com>
2020-10-30 14:35:35 +0100

    final descriptions added

--- app.js
+++ app.js
@@ -25,14 +25,14 @@ var app = express();
 //       scriptSrc: [
 //         "'self'", 
 //         "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",
-//         "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js",
+//         "https://code.jquery.com/jquery-3.4.1.slim.min.js",
 //         "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
 //         "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
 //       ],
 //       styleSrc: [
 //         "'self'", 
 //         "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",
-//         "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js",
+//         "https://code.jquery.com/jquery-3.4.1.slim.min.js",
 //         "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
 //         "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
 //       ],
